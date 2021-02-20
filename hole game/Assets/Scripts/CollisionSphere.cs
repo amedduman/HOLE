e1bb3c941e5d3e59eeffec66a5255487b4a4c5ff
amed <amedduman01@gmail.com>
2021-02-20 19:06:30 +0300

    improve visuals

---
+++ hole game/Assets/Scripts/CollisionSphere.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSphere : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("collectable"))
+        {
+            Debug.Log("collect");
+        }
+        else if (other.gameObject.CompareTag("damaging"))
+        {
+            Debug.Log( "damage");
+        }
+    }
+}
--- hole game/Assets/Scripts/HoleMovement.cs
+++ hole game/Assets/Scripts/HoleMovement.cs
@@ -7,8 +7,11 @@ public class HoleMovement : MonoBehaviour
 {
     private Mesh _groundMesh;
     private Vector3[] _meshVerts;
+    private Bounds _groundBounds;
     [SerializeField] private Transform holeCenter;
     [SerializeField] private float radius;
+    [SerializeField] [Range(.1f,1)] private float speed = 1;
+    
     private MeshCollider _meshCollider;
     
     struct VertData
@@ -22,6 +25,7 @@ public class HoleMovement : MonoBehaviour
     private void Awake()
     {
         _meshCollider = GetComponentInChildren<MeshCollider>();
+        _groundBounds = GetComponentInChildren<BoxCollider>().bounds;
     }
 
     private void Start()
@@ -32,11 +36,19 @@ public class HoleMovement : MonoBehaviour
     private void Update()
     {
         if (!Input.GetMouseButton(0)) return;
-        var x = Input.GetAxis("Mouse X");
-        var y = Input.GetAxis("Mouse Y");
+        var x = Input.GetAxis("Mouse X") * speed;
+        var y = Input.GetAxis("Mouse Y") * speed;
         var move = new Vector3(x,-y);
+        if (!IsInBounds(move)) return;
         MoveColSphere(move);
         MoveHole(move);
+    }
+
+    private bool IsInBounds(Vector3 move)
+    {
+        var pos = holeCenter.position + new Vector3(move.x,0,-move.y);
+
+        return _groundBounds.Contains(pos);
     }
 
     private void MoveColSphere(Vector3 move)
